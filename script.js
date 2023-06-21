71b397f466f5896e75ab638b9ec29cc82260d403
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-21 23:25:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const priceElements = document.querySelectorAll('.price');
+  let totalPrice = 0;
+
+  priceElements.forEach((priceElement) => {
+    const price = parseFloat(priceElement.textContent);
+    if (!isNaN(price)) {
+      totalPrice += price;
+    }
+  });
+
+  const totalRow = document.createElement('tr');
+  const totalCell = document.createElement('td');
+  totalCell.setAttribute('colspan', '2');
+  totalCell.textContent = 'Total Price: ' + totalPrice + ' Rs';
+
+  totalRow.appendChild(totalCell);
+  document.querySelector('table').appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
